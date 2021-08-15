10f9d8fc41c51c5610932115dba29a9e533a1035
EriWinckler <efwinckler@gmail.com>
2021-08-15 15:24:10 -0400

    first commit

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    public class LiarsDice {
+        Scanner scan = new Scanner(System.in);
+
+        Random randomNum = new Random();
+
+        System.out.println("Welcome to Eri's Liar Dice Game!");
+        System.out.println("How many players would like to play?");
+
+        int numPlayers = scan.nextInt(); //Getting number of players that are playing
+
+        private int user1NumDiceRemaining = 5;
+        private int user2NumDiceRemaining = 5;
+        private int user3NumDiceRemaining = 5;
+        private int user4NumDiceRemaining = 5;
+
+        private String[] numberGuess = new String[] {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX",
+                "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE"};
+
+        
+    }
+}
