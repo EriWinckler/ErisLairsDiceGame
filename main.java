039128417d062b4637ae41da09373d8fead39f8d
EriWinckler <efwinckler@gmail.com>
2021-08-15 20:00:24 -0400

    creating list of requirements and timeline

--- main.java
+++ main.java
@@ -1,3 +1,15 @@
+/*
+Steps we need to create the game:
+    1 - Set the number of players and the correspondent variables.
+    2 - Populate the dice arrays with Random dices in the 1 - 6 range.
+    3 - Ask if player want to challenge.
+        3.1 - If challenge, request number dices and value is the guess.
+    4 - Ask next player for a challenge.
+    5 - If players decide to challenge, update values of dices and guesses.
+    5.1 - If player decide not to challenge, check all the dices on the table and compare with number of dices and values guessed.
+    still working on this
+ */
+
 import java.util.*;
 
 public class main {
@@ -5,24 +17,5 @@ public class main {
 
     }
 
-    public class LiarsDice {
-        Scanner scan = new Scanner(System.in);
 
-        Random randomNum = new Random();
-
-        System.out.println("Welcome to Eri's Liar Dice Game!");
-        System.out.println("How many players would like to play?");
-
-        int numPlayers = scan.nextInt(); //Getting number of players that are playing
-
-        private int user1NumDiceRemaining = 5;
-        private int user2NumDiceRemaining = 5;
-        private int user3NumDiceRemaining = 5;
-        private int user4NumDiceRemaining = 5;
-
-        private String[] numberGuess = new String[] {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX",
-                "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE"};
-
-        
-    }
 }
